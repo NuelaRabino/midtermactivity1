92d5da30c751e102bc5f32a6384238f405d8f233
sanjuanraven15 <163710791+sanjuanraven15@users.noreply.github.com>
2024-04-10 20:04:51 +0800

    Update ShoppingCart.vue

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
+  <div class="shopping-cart">
     <h2>Shopping Cart</h2>
-    <ul>
-      <li v-for="(item, index) in cartItems" :key="index">
-        {{ item.title }} - ${{ item.price }}
-        <button @click="removeFromCart(index)">Remove</button>
-      </li>
-    </ul>
-    <p>Total: ${{ totalPrice }}</p>
+    <table>
+      <thead>
+        <tr>
+          <th>Book Name</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in cartItems" :key="index">
+          <td>{{ item.title }}</td>
+          <td>{{ item.quantity }}</td>
+          <td>₱{{ item.price }}</td>
+          <td><button class="remove-button" @click="removeFromCart(index)">Remove</button></td>
+        </tr>
+      </tbody>
+    </table>
+    <p>Total: ₱{{ totalPrice }}</p>
   </div>
 </template>
 
