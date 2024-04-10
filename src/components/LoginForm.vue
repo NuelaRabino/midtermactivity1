1304e6b548d7bb351e784fe5c24804f29cf21b16
sanjuanraven15 <163710791+sanjuanraven15@users.noreply.github.com>
2024-04-10 20:05:27 +0800

    Update LoginForm.vue

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,35 +1,35 @@
 <template>
-    <div>
-      <h2>Login</h2>
-      <form @submit.prevent="login">
-        <label>Username: </label>
-        <input type="text" v-model="username" required>
-        <label>Password: </label>
-        <input type="password" v-model="password" required>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  import { mapActions } from 'vuex'
-  
-  export default {
-    data() {
-      return {
-        username: '',
-        password: ''
-      }
-    },
-    methods: {
-      ...mapActions(['authenticate']),
-      login() {
-        this.authenticate({ username: this.username, password: this.password })
-        // Clear the input fields after login attempt
-        this.username = ''
-        this.password = ''
-      }
+  <div class="login-container">
+    <h2>Login</h2>
+    <form @submit.prevent="login" class="login-form">
+      <label for="username">Username:</label>
+      <input id="username" type="text" v-model="username" required>
+      <label for="password">Password:</label>
+      <input id="password" type="password" v-model="password" required>
+      <br>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    ...mapActions(['authenticate']),
+    login() {
+      this.authenticate({ username: this.username, password: this.password })
+      // Clear the input fields after login attempt
+      this.username = ''
+      this.password = ''
     }
   }
-  </script>
-  
+}
+</script>
