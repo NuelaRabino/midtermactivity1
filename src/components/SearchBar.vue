ce034bf7218469dd061a44f3111b264f701ef58b
sanjuanraven15 <163710791+sanjuanraven15@users.noreply.github.com>
2024-04-10 20:05:12 +0800

    Update SearchBar.vue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,28 +1,54 @@
 <template>
-    <div>
-      <h2>Search Books</h2>
-      <form @submit.prevent="searchBook">
-        <input type="text" v-model="searchQuery" placeholder="Enter book title...">
-        <button type="submit">Search</button>
-      </form>
+  <div>
+    <h2>Search Books</h2>
+    <form @submit.prevent="searchBook">
+      <input type="text" v-model="searchQuery" placeholder="Enter book title..."> |
+      <button type="submit">Search</button>
+    </form>
+
+    <!-- Display search results in table format -->
+    <div v-if="filteredBooks.length > 0">
+      <h3>Search Results</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Book Name</th>
+            <th>Price</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(book, index) in filteredBooks" :key="index">
+            <td>{{ book.title }}</td>
+            <td>₱{{ book.price }}</td>
+            <td><button @click="addToCart(book)">Add to Cart</button></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        searchQuery: ''
-      };
-    },
-    methods: {
-      searchBook() {
-        // Tawagin ang isang action na i-search ang mga aklat batay sa searchQuery
-        // Halimbawa:
-        // this.$store.dispatch('searchBooks', this.searchQuery);
-        this.$store.commit('setSearchQuery', this.searchQuery.trim());
-      }
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: ''
+    };
+  },
+  computed: {
+    filteredBooks() {
+      return this.$store.getters.filteredBooks;
     }
-  };
-  </script>
-  
+  },
+  methods: {
+    searchBook() {
+      this.$store.commit('setSearchQuery', this.searchQuery.trim());
+    },
+    addToCart(book) {
+      this.$store.commit('addToCart', book);
+    }
+
+  }
+};
+</script>
