0c818c03457c2993b2f2facfe55e3661580e394e
sanjuanraven15 <163710791+sanjuanraven15@users.noreply.github.com>
2024-04-10 20:05:50 +0800

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,25 +1,34 @@
 <template>
-    <div>
-      <h2>Available Books</h2>
-      <ul>
-        <li v-for="book in books" :key="book.id">
-          {{ book.title }} - ${{ book.price }}
-          <button @click="addToCart(book)">Add to Cart</button>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import { mapState, mapMutations } from 'vuex'
-  
-  export default {
-    computed: {
-      ...mapState(['books'])
-    },
-    methods: {
-      ...mapMutations(['addToCart'])
-    }
+  <div class="book-list">
+    <h2>Available Books</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Book Name</th>
+          <th>Price</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="book in books" :key="book.id">
+          <td>{{ book.title }}</td>
+          <td>₱{{ book.price }}</td>
+          <td><button @click="addToCart(book)">Add to Cart</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['books'])
+  },
+  methods: {
+    ...mapMutations(['addToCart'])
   }
-  </script>
-  
+}
+</script>
